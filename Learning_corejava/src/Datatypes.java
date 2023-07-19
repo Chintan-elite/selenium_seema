e36869578fb98f11ac4adc4605712cafc3902da7
chintan <chintan.tops@gmail.com>
2023-07-19 08:48:03 +0530

    oprators

--- Learning_corejava/src/Datatypes.java
+++ Learning_corejava/src/Datatypes.java
@@ -2,21 +2,38 @@
 public class Datatypes {
 	public static void main(String[] args) {
 		
-		 byte b = -128;
-		 short s = 32767;
-		 int i = 2656;
-		 long l = 565656565;
-		 
-		float f = 45.56568989891f;
-		System.out.println(f);
-		double d = 4646.5656565656565;
-		System.out.println(d);
+//		 byte b = -128;
+//		 short s = 32767;
+//		 int i = 2656;
+//		 long l = 565656565;
+//		 
+//		float f = 45.56568989891f;
+//		System.out.println(f);
+//		double d = 4646.5656565656565;
+//		System.out.println(d);
+//		
+//		char ch = 'A';
+//		System.out.println(ch);
+//		
+//		boolean bool = true;
+//		System.out.println(bool);
 		
-		char ch = 'A';
+		//String name = "Seema";
+		
+		//widning - implicite
+		int k = 10;  //4 byte
+		long l = k;  //8 byte
+		
+		//nerrowing - explicite
+		long x = 56565;       //8 byte
+		int y = (int) x;      //4 byte
+		
+		char ch = 109;
 		System.out.println(ch);
 		
-		boolean bool = true;
-		System.out.println(bool);
+		int i = 'X';
+		System.out.println(i);
+		
 	}
 	
 	
---
+++ Learning_corejava/src/oprators/O005_Turnary.java
@@ -0,0 +1,14 @@
+package oprators;
+
+public class O005_Turnary {
+	public static void main(String[] args) {
+		
+		int a = 100;
+		int b = 20;
+		
+		String op =  (a>b) ? "A is greater" : "B is greater ";
+		
+		System.out.println(op);
+		
+	}
+}
